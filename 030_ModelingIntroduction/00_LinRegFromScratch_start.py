d7c1dbe52192c452086c4f918e831531bf5b0329
GastCre <gcrecike@gmail.com>
2025-04-24 21:59:14 +0200

    Implement Linear Regression model class: add forward method, loss function, and training loop with optimizer

--- 030_ModelingIntroduction/00_LinRegFromScratch_start.py
+++ 030_ModelingIntroduction/00_LinRegFromScratch_start.py
@@ -15,15 +15,46 @@ sns.scatterplot(x='wt', y='mpg', data=cars)
 sns.regplot(x='wt', y='mpg', data=cars)
 
 #%% convert data to tensor
+X_list=cars.wt.values
+X_np = np.array(X_list, dtype=np.float32).reshape(-1,1)
+y_list=cars.mpg.values.tolist()
+X=torch.from_numpy(X_np)
+y=torch.tensor(y_list)
 
+#%% training for linear regression y = x*w + b
+w = torch.rand(1,requires_grad=True, dtype=torch.float32)
+b = torch.rand(1,requires_grad=True, dtype=torch.float32)
 
-#%% training
+num_epochs= 1000
+learning_rate= 0.001
 
 #%% check results
-# %%
-
+for epoch in range(num_epochs):
+    for i in range(len(X)):
+        # Forward pass
+        y_pred = X[i]*w + b
+        # Loss function
+        loss_tensor = torch.pow(y_pred - y[i],2)
+        # Backwards pass
+        loss_tensor.backward()
+        # Extract losses
+        loss_value=loss_tensor.data[0]
+        # Update weights and biasses
+        with torch.no_grad():
+            w -= w.grad * learning_rate
+            b -= b.grad * learning_rate
+            w.grad.zero_()
+            b.grad.zero_()
+print("y = x*w + b")
+print(f"Weight (w): {w.item()}, Bias (b): {b.item()}")
+#%%
+y_pred = ((X*w)+b).detach().numpy()
+sns.scatterplot(x=X_list, y=y_list)
+sns.lineplot(x=X_list, y=y_pred.reshape(-1))
 # %% (Statistical) Linear Regression
-
+from sklearn.linear_model import LinearRegression
+reg=LinearRegression().fit(X_np,y_list)
+print(f"Slope (w): {reg.coef_}, Intercept (b): {reg.intercept_}")
 
 # %% create graph visualisation
 # make sure GraphViz is installed (https://graphviz.org/download/)
--- 030_ModelingIntroduction/10_LinReg_ModelClass_start.py
+++ 030_ModelingIntroduction/10_LinReg_ModelClass_start.py
@@ -23,3 +23,56 @@ X = torch.from_numpy(X_np)
 y_true = torch.from_numpy(y_np)
 
 #%% model class
+class LinearRegressionTorch(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(LinearRegressionTorch,self).__init__()
+        self.linear = nn.Linear(input_size, output_size) #linear layer 
+    def forward(self,x):
+        out=self.linear(x)
+        return out
+
+input_dim = 1
+output_dim=1
+model=LinearRegressionTorch(input_dim,output_dim)
+
+# %% Loss function
+loss_funct=nn.MSELoss()
+#%% Optimizer
+LR=0.02 
+#Stochastic Gradient Descent
+optimizer=torch.optim.SGD(model.parameters(),lr=LR)
+# %% Training
+losses, slope, bias = [],[],[]
+NUM_EPOCHS=1000
+
+for epoch in range(NUM_EPOCHS):
+    # We set the gradients to zero
+    optimizer.zero_grad()
+    #Forward pass
+    y_pred=model(X)
+    # Loss function
+    loss=loss_funct(y_pred, y_true)
+    loss.backward()
+    #Update weights
+    optimizer.step()
+    #Get parameters
+    for name, param in model.named_parameters():
+        if param.requires_grad:
+            if name =='linear.weight':
+                slope.append(param.data.numpy()[0][0])
+            if name == 'linear.bias':
+                bias.append(param.data.numpy()[0])
+    # Store loss
+    losses.append(float(loss.data))
+    # Print loss
+    if epoch % 100 ==0:
+        print('Epoch: {}, Loss: {:.4f}'.format(epoch, loss.data))
+# %%
+sns.scatterplot(x=range(NUM_EPOCHS),y=losses)
+# %%
+sns.scatterplot(x=range(NUM_EPOCHS),y=bias)
+
+# %%
+sns.scatterplot(x=range(NUM_EPOCHS),y=slope)
+
+# %%
