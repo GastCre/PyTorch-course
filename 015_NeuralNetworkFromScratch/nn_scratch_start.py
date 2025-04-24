8316024519749c94b407f134d323b2f892ad7158
GastCre <gcrecike@gmail.com>
2025-04-24 20:14:13 +0200

    Refactor NeuralNetwork class: implement forward, backward, and optimizer methods; enhance training process

--- 015_NeuralNetworkFromScratch/nn_scratch_start.py
+++ 015_NeuralNetworkFromScratch/nn_scratch_start.py
@@ -28,7 +28,7 @@ X_test_scale = scaler.transform(X_test)
 
 #%% network class
 class NeuralNetwork:
-    def __init__(self, LR, X_train, y_train,X_test, y_test):
+    def __init__(self, LR, X_train, y_train, X_test, y_test):
         self.w=np.random.randn(X_train.shape[1])
         self.b=np.random.randn()
         self.LR=LR
@@ -42,25 +42,81 @@ class NeuralNetwork:
     def activation(self,x):
         return 1/(1+np.exp(-x)) #Sigmoid function
     
-    def der_activation(self,x):
+    def dactivation(self,x):
         return self.activation(x)*(1-self.activation(x))
+    
     def forward(self,X):
         hidden_1=np.dot(X,self.w)+self.b
         activate_1=self.activation(hidden_1)
         return activate_1
     
-
+    def backward(self, X, y_true):
+        hidden_1=np.dot(X,self.w)+self.b
+        y_pred=self.forward(X)
+        # Derivative of the loss function L=(y_pred-y_true)^2.
+        dL_dpred=2*(y_pred-y_true)
+        dpred_dh1=self.dactivation(hidden_1)
+        dh1_db=1
+        dh1_dw=X
+        #Derivative of the loss function wrt w and b
+        #in order to optimize the weights
+        dL_db=dL_dpred*dpred_dh1*dh1_db
+        dL_dw=dL_dpred*dpred_dh1*dh1_dw
+        return dL_db, dL_dw
+    
+    def optimizer(self, dL_db,dL_dw):
+        self.b=self.b- dL_db * self.LR
+        self.w=self.w- dL_dw * self.LR
+    
+    def train(self,ITERATIONS):
+        for i in range(ITERATIONS):
+            # random position
+            random_pos=np.random.randint(len(self.X_train))
+            # forward path
+            y_train_true=self.y_train[random_pos]
+            y_train_pred=self.forward(X_train[random_pos])
+            # calculate training losses
+            L=np.square(y_train_pred-y_train_true)
+            self.L_train.append(L)
+            #Calculate gradients
+            dL_db, dL_dw = self.backward(self.X_train[random_pos], self.y_train[random_pos])
+            #update the weights
+            self.optimizer(dL_db,dL_dw)
+            #calulcate error for test data
+            L_sum=0
+            for j in range(len(self.X_test)):
+                y_true = self.y_test[j]
+                y_pred = self.forward(self.X_test[j])
+                L_sum += np.square(y_pred - y_true)
+            self.L_test.append(L_sum)
+        return "Training succesful!"
+        
 #%% Hyper parameters
-
+LR = 0.1
+ITERATIONS = 1000
 #%% model instance and training
-
+nn = NeuralNetwork(LR=LR, X_train=X_train_scale, y_train=y_train,X_test=X_test_scale, y_test=y_test)
+nn.train(ITERATIONS=ITERATIONS)
 # %% check losses
-
+sns.lineplot(x=list(range(len(nn.L_test))),y=nn.L_test)
 # %% iterate over test data
+total = X_test_scale.shape[0]
+correct = 0
+y_preds=[]
+for i in range(total):
+    y_true= y_test[i]
+    y_pred = np.round(nn.forward(X_test_scale[i]))
+    y_preds.append(y_pred)
+    correct += 1 if y_true == y_pred else 0
 
 # %% Calculate Accuracy
-
-# %% Baseline Classifier
-
+correct/total
+# %% In order to check if the model
+# performs better than just simly using
+# the class distribution to estimate the outcome,
+# we will construct a baseline Classifier
+from collections import Counter
+Counter(y_test)
 # %% Confusion Matrix
-
+conf_matrix=confusion_matrix(y_true=y_test, y_pred=y_preds)
+# %%
