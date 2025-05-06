bf80cee2c855b3b0b93b38d87fcf32f27f8e7317
GastCre <gcrecike@gmail.com>
2025-05-06 09:41:27 +0200

    Refactor image loading path and enhance preprocessing steps for consistency; update image mean and std calculation

--- 060_CNN_ImageClassification/BinaryClassification/CNN_BinaryClassification_start.py
+++ 060_CNN_ImageClassification/BinaryClassification/CNN_BinaryClassification_start.py
@@ -4,6 +4,7 @@ import torchvision
 import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.data import DataLoader
 import os
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,6 +12,21 @@ from sklearn.metrics import accuracy_score
 os.getcwd()
 
 #%% transform, load data
+transform = transforms.Compose([
+    transforms.Resize(32),
+    transforms.Grayscale(num_output_channels=1),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5),(0.5))
+])
+
+batch_size=4
+trainset=torchvision.datasets.ImageFolder(root='/Users/gastoncrecikeinbaum/Documents/Data Science/Courses/PyTorch/PyTorch-course/060_CNN_ImageClassification/BinaryClassification/data/train',
+                                          transform=transform)
+testset=torchvision.datasets.ImageFolder(root='/Users/gastoncrecikeinbaum/Documents/Data Science/Courses/PyTorch/PyTorch-course/060_CNN_ImageClassification/BinaryClassification/data/test',
+                                          transform=transform)
+trainloader=DataLoader(dataset=trainset,batch_size=batch_size,shuffle=True)
+testloader=DataLoader(dataset=testset,batch_size=batch_size,shuffle=True)
+
 
 # %% visualize images
 def imshow(img):
@@ -22,39 +38,67 @@ def imshow(img):
 
 # get some random training images
 dataiter = iter(trainloader)
-images, labels = dataiter.next()
+images, labels = next(dataiter)
 imshow(torchvision.utils.make_grid(images, nrow=2))
 # %% Neural Network setup
 class ImageClassificationNet(nn.Module):
     def __init__(self) -> None:
-        pass
+        # Output size = ((Input Size - Kernel Size + 2 × Padding) / Stride) + 1
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 6, 3) # 1: Grayscale, 6: filters/kernels, 3: kernel size (3x3). Out: batch size 6,30,30 (30 because of the kernel size)
+        self.pool = nn.MaxPool2d(2,2) # batch size: 6, 15, 15 (we reduce dim by half)
+        self.conv2 = nn.Conv2d (6, 16, 3) # Increase channels from 6 to 16. batch size: 16, 13, 13. Same as above: 15-3+1=13
+        self.fc1 = nn.Linear(6*6*16, 128) #after next pool: 16, 6 ,6
+        self.fc2 = nn.Linear(128,64) #Now we reduce features to 64
+        self.fc3 = nn.Linear(64,1) # and finally 1 for output (0/1)
+        self.sigmoid=nn.Sigmoid()
+        self.relu=nn.ReLU()
+        
     
     def forward(self, x):
-        pass
+        x=self.conv1(x) #out: 6, 30, 30
+        x=F.relu(x)
+        x=self.pool(x) #out: 6, 15, 15
+        x=self.conv2(x) #out: 16, 13, 13
+        x=F.relu(x)
+        x=self.pool(x) #out: 16, 6, 6
+        x=torch.flatten(x,1) #Reduce to 1 dim: 16*6*6
+        x=self.fc1(x) #out: 128
+        x=self.relu(x)
+        x=self.fc2(x) #out: 64
+        x=self.relu(x)
+        x=self.fc3(x) #out: 1
+        x=self.sigmoid(x)
+        return x
 
 #%% init model
 model = ImageClassificationNet()      
-# loss_fn = ...
-# optimizer = ...
+loss_fn = nn.BCELoss()
+optimizer = torch.optim.SGD(model.parameters(),lr=0.001, momentum=0.8)
 # %% training
+losses=[]
 NUM_EPOCHS = 10
 for epoch in range(NUM_EPOCHS):
     for i, data in enumerate(trainloader, 0):
         inputs, labels = data
         # zero gradients
-        
+        optimizer.zero_grad
         # forward pass
-        
+        outputs=model(inputs)
         # calc losses
-        
+        loss=loss_fn(outputs,labels.reshape(-1,1).float()) #Have to reshape the tensor
         # backward pass
-        
-
+        loss.backward()
         # update weights
-        
+        optimizer.step()
         if i % 100 == 0:
             print(f'Epoch {epoch}/{NUM_EPOCHS}, Step {i+1}/{len(trainloader)},'
                   f'Loss: {loss.item():.4f}')
+    losses.append(float(loss.item()))
+#%%
+import seaborn as sns
+sns.lineplot(x=range(NUM_EPOCHS),y=losses)
+
 # %% test
 y_test = []
 y_test_pred = []
--- 060_CNN_ImageClassification/ImagePreprocessing_start.py
+++ 060_CNN_ImageClassification/ImagePreprocessing_start.py
@@ -5,7 +5,21 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 # %% import image
-img = Image.open('kiki.jpg')
+img = Image.open('/Users/gastoncrecikeinbaum/Documents/Data Science/Courses/PyTorch/PyTorch-course/060_CNN_ImageClassification/kiki.jpg')
 img
 
 # %% compose a series of steps
+# better to use the same number in the tuple to keep dimensions equal
+preprocess_steps=transforms.Compose([
+    transforms.Resize((300,300)),
+    transforms.RandomRotation(50),
+    transforms.CenterCrop(200),
+    transforms.Grayscale(),
+    transforms.RandomVerticalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5],std=[0.5])
+    ])
+x=preprocess_steps(img)
+# %% get the mean and std of a given imahe
+x.mean(), x.std()
+# %%
