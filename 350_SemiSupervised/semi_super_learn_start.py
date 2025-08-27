95ce983a9fef29ac662c841c921619c37e310f77
GastCre <gcrecike@gmail.com>
2025-08-27 11:28:37 +0200

    Refactor UnlabeledDataset class and update dataset paths for consistency

--- 350_SemiSupervised/semi_super_learn_start.py
+++ 350_SemiSupervised/semi_super_learn_start.py
@@ -22,45 +22,43 @@ transform_super = transforms.Compose(
     transforms.ToTensor(),
     transforms.Normalize((0.5, ), (0.5, ))])
 
-#%% Class for Unlabeled Dataset
+#%% TODO: Class for Unlabeled Dataset
 class UnlabeledDataset(Dataset):
-    
-    sesemi_transformations = {0: 0, 1: 90, 2: 180, 3: 270}
-    
-    def __init__(self, folder_path):
+
+    sesemi_transformations ={0: 0, 1: 90, 2: 180, 3: 270}
+
+    def __init__(self,folder_path):
         super().__init__()
-        self.folder_path = folder_path
-        self.image_names = os.listdir(folder_path)
-        self.images_full_path_names = [f"{folder_path}/{i}" for i in self.image_names]    
-    
+        self.folder_path=folder_path
+        self.image_names=os.listdir(folder_path)
+        self.images_full_path_names=[f"{folder_path}/{i}" for i in self.image_names]
+        
     def __len__(self):
         return len(self.image_names)
     
-    def __getitem__(self, idx):
+    def __getitem__(self,idx):
         img = Image.open(self.images_full_path_names[idx])
         # get a random sesemi transformation
-        transformation_class_label = random.randint(0, len(self.sesemi_transformations)-1)
-        # apply the randomly selected transformation
+        transformation_class_label = random.randint(0,len(self.sesemi_transformations))
+        #apply the randomly selected transformation
         angle = self.sesemi_transformations[transformation_class_label]
         data = transform_super(img)
-        data = transforms.functional.rotate(img=data, angle=angle)
+        data = transforms.function.rotate(img=data,angle=angle)
         return data, transformation_class_label
-    
-# %% Dataset for unlabeled data
-folder_path = 'data/unlabeled'
-unlabeled_ds = UnlabeledDataset(folder_path)
 
+#%% TODO: Dataset for unlabeled data
+folder_path = "/Users/gastoncrecikeinbaum/Documents/Data Science/Courses/PyTorch/PyTorch-course/350_SemiSupervised/data/unlabeled"
+unlabeled_ds=UnlabeledDataset(folder_path)
 
 #%% Dataset for train and test
-train_ds = torchvision.datasets.ImageFolder(root='data/train', transform=transform_super)
-test_ds = torchvision.datasets.ImageFolder(root='data/test', transform=transform_super)
+train_ds = torchvision.datasets.ImageFolder(root='/Users/gastoncrecikeinbaum/Documents/Data Science/Courses/PyTorch/PyTorch-course/350_SemiSupervised/data/train', transform=transform_super)
+test_ds = torchvision.datasets.ImageFolder(root='/Users/gastoncrecikeinbaum/Documents/Data Science/Courses/PyTorch/PyTorch-course/350_SemiSupervised/data/test', transform=transform_super)
 # %% Dataloaders Supervised
 train_loader = DataLoader(train_ds, batch_size=BATCH_SIZE)
 test_loader = DataLoader(test_ds, batch_size=BATCH_SIZE)
 
 #%% TODO: Dataloader Unsupervised
-unlabeled_loader = DataLoader(unlabeled_ds, batch_size=BATCH_SIZE)
-
+unlabeled_loader=DataLoader(unlabeled_ds,batch_size=BATCH_SIZE)
 #%% Model Class
 class SesemiNet(nn.Module):
     def __init__(self, n_super_classes, n_selfsuper_classes) -> None:
@@ -92,14 +90,16 @@ class SesemiNet(nn.Module):
     
     # TODO: update forward pass
     def forward(self, x_supervised, x_selfsupervised):
+        #Supervised branch
         x_supervised = self.backbone(x_supervised)
         x_supervised = self.fc_out_super(x_supervised)
         x_supervised = self.output_layer_super(x_supervised)
-        
+        #Self-supervised branch
         x_selfsupervised = self.backbone(x_selfsupervised)
         x_selfsupervised = self.fc_out_selfsuper(x_selfsupervised)
         x_selfsupervised = self.output_layer_selfsuper(x_selfsupervised)
-        return x_supervised, x_selfsupervised                
+        
+
     
 model = SesemiNet(n_super_classes=2, n_selfsuper_classes=4)
 model.train()
@@ -114,8 +114,7 @@ train_losses_self = []
 for epoch in range(NUM_EPOCHS):
     train_loss = 0
     # TODO: get data
-    data_loaders = zip(train_loader, unlabeled_loader)
-    
+   
     for i, (supervised_data, selfsupervised_data) in enumerate(data_loaders):
         X_super, y_super = supervised_data
         X_selfsuper, y_selfsuper = selfsupervised_data
@@ -127,9 +126,7 @@ for epoch in range(NUM_EPOCHS):
         y_super_pred, y_selfsuper_pred = model(X_super, X_selfsuper)
               
         # TODO: calc losses
-        loss_super = criterion_supervised(y_super_pred, y_super)
-        loss_selfsuper = criterion_selfsupervised(y_selfsuper_pred, y_selfsuper)
-        loss = loss_super + loss_selfsuper * LOSS_FACTOR_SELFSUPERVISED
+    
         
         # calculate gradients
         loss.backward()
