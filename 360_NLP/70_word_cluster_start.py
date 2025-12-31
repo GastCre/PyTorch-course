19deb28b90c8ce2c0e77a0ca1bf1d3a356eefc72
GastCre <gcrecike@gmail.com>
2025-12-31 16:27:53 +0100

    Refactor code formatting and improve readability in 70_word_cluster_start.py

--- 360_NLP/70_word_cluster_start.py
+++ 360_NLP/70_word_cluster_start.py
@@ -1,25 +1,62 @@
-#%% packages
+# %% packages
 import pandas as pd
 from plotnine import ggplot, aes, geom_text, labs
 from sklearn.manifold import TSNE
 import torchtext.vocab as vocab
 import torch
 
-#%% import GloVe
+# %% import GloVe
 glove_dim = 100
-glove = vocab.GloVe(name='6B', dim = glove_dim)
+glove = vocab.GloVe(name='6B', dim=glove_dim)
 
-#%% Get closest words from word input
+# %% Get closest words from word input
+
+
 def get_embedding_vector(word):
     word_index = glove.stoi[word]
     emb = glove.vectors[word_index]
     return emb
 
+
 def get_closest_words_from_word(word, max_n=5):
     word_emb = get_embedding_vector(word)
-    distances = [(w, torch.dist(word_emb, get_embedding_vector(w)).cpu().item()) for w in glove.itos]
+    distances = [(w, torch.dist(word_emb, get_embedding_vector(w)
+                                ).cpu().item()) for w in glove.itos]
     dist_sort_filt = sorted(distances, key=lambda x: x[1])[:max_n]
     return [item[0] for item in dist_sort_filt]
 
+
 get_closest_words_from_word(word='chess', max_n=10)
 
+
+# %%
+words = []
+categories = ['numbers', 'science', 'technology', 'cooking', 'music']
+
+df_word_cloud = pd.DataFrame({
+    'category': [],
+    'word': []
+})
+
+for category in categories:
+    print(category)
+    closest_words = get_closest_words_from_word(word=category, max_n=20)
+    temp = pd.DataFrame({
+        'category': [category]*len(closest_words),
+        'word': closest_words
+    }
+    )
+    df_word_cloud = pd.concat([df_word_cloud, temp], ignore_index=True)
+
+# %% get the 100 dimension word embedding for all words
+
+n_rows = df_word_cloud.shape[0]
+n_cols = glove_dim
+X = torch.empty((n_rows, n_cols))
+
+for i in range(n_rows):
+    current_word = df_word_cloud.iloc[i, 'word']
+    X[i, :] = get_embedding_vector(current_word)
+    print(f"{i}: {current_word}")
+
+# %%
